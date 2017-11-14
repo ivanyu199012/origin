b2acef42d1776db6e77b293c02aeb018d531a708
WIN-2SEM4BKULD9\Administrator <chuenyuen027546@gmail.com>
2017-11-14 17:51:11 +0900

    Testing mongo db
    1. Install library mongodb, Q and clone
    2. Make an example of updating a record in mongodb, with using cloning and promise

--- ExcelTest/app.js
+++ ExcelTest/app.js
@@ -1,5 +1,7 @@
 ( function() 
 {
+    // Testing excel library
+    /*
     var cusXLSX = require( './cusXLSX.js' );
 
     var desired_value = cusXLSX.readFirstCellInFile( '\\\\13.197.147.195\\Reporting\\HK Reporting.xlsx' );
@@ -7,4 +9,45 @@
 
     var testMsg = 'Test for BBBBBBBBBBBBBBB';
     console.log( testMsg );
+    */
+
+    // Testing mongo db
+    var Q = require('q');
+    var clone = require('clone');
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://sa:x-admin00@localhost:27017/ICMS?authSource=admin";
+
+    MongoClient.connect(url, function(err, db) 
+    {
+        if (err) throw err;
+
+        console.log("Current DB:" + db.databaseName);
+    
+
+        var deferred = Q.defer();
+
+        db.collection("test").findOne({}, function(err, result) 
+        {
+            if (err) throw err;
+            
+            deferred.resolve( result );
+        });
+
+        deferred.promise.then( function( record )
+        {
+            var oldRecord = clone( record );
+            record.description = 'No description';
+            db.collection("test").updateOne( 
+            oldRecord, 
+            record,
+            function()
+            {
+                if (err) throw err;
+                
+                console.log("1 document updated");
+
+                db.close();
+            } );
+        });
+    });
 })();
