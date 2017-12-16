b2cbdd7e76b0c0a2184d4d3748f983f9dea9c65b
WIN-2SEM4BKULD9\Administrator <chuenyuen027546@gmail.com>
2017-12-16 18:53:40 +0900

    Add passport.js and mongoose Did not apply bluebird yet

--- myExpressApp/routes/index.js
+++ myExpressApp/routes/index.js
@@ -1,13 +1,92 @@
-var express = require( 'express' );
-var router = express.Router();
-var wwwObj = require( '../bin/www' );
-var io = require('socket.io')( wwwObj.server );
+const express = require( 'express' );
+const passport = require('passport');
+const Account = require('../models/account');
+const io = require('socket.io')( require( '../bin/www' ).server );
+const router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) 
 {
-  res.render('index', { title : 'Express' });
+  res.render('index', 
+	{ 
+		title : 'Express', 
+		user : req.user 
+	});
 });
+
+router.get('/register', function( req, res )
+{
+    res.render('register', { });
+});
+
+router.post('/register', function(req, res, next)
+{
+    Account.register(
+        new Account({ username : req.body.username }), 
+        req.body.password, 
+        function(err, account)
+		{
+			
+			if (err) {
+			  return res.render('register', { error : err.message });
+			}
+
+			passport.authenticate('local')( req, res, function()
+			{
+				req.session.save( function(err)
+				{
+					if (err) 
+					{
+						return next(err);
+					}
+					res.redirect('/');
+				});
+			});
+		});
+});
+
+
+router.get('/login', function(req, res)
+{
+	if ( req.user )
+	{
+		res.redirect('/');
+	}
+	else
+	{
+		res.render('login', { user : req.user, error : req.flash('error')});
+	}
+});
+
+router.post('/login', 
+	passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), function(req, res, next)
+	{
+		req.session.save( function( err )
+		{
+			if (err) {
+				return next(err);
+			}
+			res.redirect('/');
+		});
+    });
+
+
+router.get('/logout', 
+	function(req, res, next)
+	{
+		req.logout();
+		req.session.save(
+		function(err)
+		{
+			if (err) {
+				return next(err);
+			}
+			res.redirect('/');
+		});
+	});
+
+
 
 // Test post
 router.post('/test/', function(req, res, next) 
